5fb3748ff6a26bdbed0fbaf24a9b411330a56aef
Carolina Neponucena <123747829+carolneponucenaa@users.noreply.github.com>
2024-04-11 11:16:27 -0300

    finalizado

--- app/src/main/java/br/senai/jandira/sp/screens/TelaHome.kt
+++ app/src/main/java/br/senai/jandira/sp/screens/TelaHome.kt
@@ -3,17 +3,23 @@ package br.senai.jandira.sp.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -29,6 +35,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,11 +59,62 @@ fun TelaHome(){
             shape = RectangleShape
         )
         {
-            Image(
-                painter = painterResource(id = R.drawable.paisagem),
-                contentDescription = "",
-                contentScale = ContentScale.Crop
-            )
+            Box(modifier = Modifier.fillMaxSize()){
+                Image(
+                    painter = painterResource(id = R.drawable.paisagem),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop
+                )
+                Row(modifier = Modifier
+                    .padding(bottom = 50.dp)
+                    .align(Alignment.BottomStart)
+                ){
+                    Icon(
+                        imageVector = Icons.Default.LocationOn,
+                        contentDescription = "Botao de Busca",
+                        tint = Color.White
+                    )
+                    Text(text = "YOU'RE IN PARIS",
+                        color = Color.White,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Normal,
+                        fontFamily = FontFamily.SansSerif
+                    )
+                }
+                Box(modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomStart)){
+                    Text(text = "My Trips",
+                        color = Color.White,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif
+                    )
+                }
+                Box(modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.TopEnd)){
+                    Card(
+                        modifier = Modifier
+                            .size(90.dp),
+                        shape = CircleShape
+                    ){
+                        Image(painter = painterResource(id = R.drawable.mulher),
+                            contentDescription = "Foto de Perfil",
+                            contentScale = ContentScale.Crop,
+                            )
+                    }
+                        Column (
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+                            Spacer(modifier = Modifier.height(85.dp))
+                            Text(
+                                text = "Susanna Hoffs",
+                                color = Color.White,
+                                fontWeight = FontWeight.Light)
+                        }
+                }
+            }
         }
         Column(
             modifier = Modifier
@@ -66,6 +124,7 @@ fun TelaHome(){
         ) {
             Column(
                 modifier = Modifier
+                    .padding(horizontal = 3.dp)
                     .fillMaxWidth(),
             ) {
                 Text(
@@ -75,7 +134,7 @@ fun TelaHome(){
                     color = Color.Gray
                 )
                 LazyRow(modifier = Modifier.fillMaxWidth()){
-                    items(2) {
+                    items(3) {
                         Card (
                             modifier = Modifier
                                 .padding(10.dp)
@@ -85,28 +144,38 @@ fun TelaHome(){
                                 .cardColors(containerColor = Color.Magenta
                                 )
                         ){
-                            Image(
-                                painter = painterResource(id = R.drawable.montain),
-                                contentDescription = "",
-                                contentScale = ContentScale.Crop
-                            )
-                            Text(
-                                text = "Montain",
-                                fontSize = 16.sp,
-                                color = Color.White
-                            )
+                            Column (
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ){
+                                Image(
+                                    painter = painterResource(id = R.drawable.mountain),
+                                    contentDescription = "",
+                                    contentScale = ContentScale.Crop
+                                )
+                                Text(
+                                    text = "Mountain",
+                                    fontSize = 16.sp,
+                                    color = Color.White,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+
                         }
                     }
                 }
 
             Row (
-                modifier = Modifier.width(350.dp),
+                modifier = Modifier
+                    .width(350.dp)
+                    .padding(horizontal = 20.dp),
                 horizontalArrangement = Arrangement.Center
             ){
                 OutlinedTextField(value = "",
                     onValueChange ={},
                     modifier = Modifier
-                        .background(Color.White)
+                        .background(Color.White, RoundedCornerShape(30))
                         .fillMaxWidth(),
                     colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = Color.White,
@@ -115,7 +184,8 @@ fun TelaHome(){
                     trailingIcon = {
 
                         IconButton(onClick = { /*TODO*/ }) {
-                            Icon(imageVector = Icons.Default.Search,
+                            Icon(
+                                imageVector = Icons.Default.Search,
                                 contentDescription = "Botao de Busca",
                                 tint = Color.Gray
                             )
@@ -140,7 +210,7 @@ fun TelaHome(){
             LazyColumn (
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(2){
+                items(3){
                     Card (
                         modifier = Modifier
                             .padding(10.dp)
@@ -164,7 +234,7 @@ fun TelaHome(){
                             )
                             {
                                 Image(
-                                    painter = painterResource(id = R.drawable.paisagem),
+                                    painter = painterResource(id = R.drawable.londres),
                                     contentDescription = "",
                                     contentScale = ContentScale.Crop
                                 )
@@ -184,7 +254,18 @@ fun TelaHome(){
                                     color = Color.Gray
                                 )
                             }
-
+                                Row(
+                                    modifier = Modifier.width(350.dp),
+                                    horizontalArrangement = Arrangement.End,
+                                    verticalAlignment = Alignment.Bottom
+                                ){
+                                    Text(
+                                        text = "18 Feb - 21 Feb",
+                                        fontSize = 12.sp,
+                                        color = Color.Magenta,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
                         }
                     }
                 }
